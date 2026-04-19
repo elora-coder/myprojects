cfcf435655bac9ebd35e9fce8a2b4f20216b4673
elora-coder <kilaurafan@gmail.com>
2026-04-19 20:55:52 +0300

    Add files via upload

---
+++ lab8/src/components/NavBar.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps(["lang"]);
+</script>
+
+<template>
+  <nav>
+    <ul>
+      <li>
+        <router-link to="/" exact-active-class="active-link">Головна</router-link>
+      </li>
+
+      <li><router-link :to="{ path: '/', hash: '#about' }">Про мене</router-link></li>
+      <li><router-link :to="{ path: '/', hash: '#skills' }">Навички</router-link></li>
+      <li><router-link :to="{ path: '/', hash: '#education' }">Освіта</router-link></li>
+
+      <li>
+        <router-link to="/contacts" active-class="active-link">Контакти</router-link>
+      </li>
+      
+      <li><a href="/myprojects/index.html">{{ lang === "uk" ? "Проєкти" : "Project" }}</a></li>
+    </ul>
+  </nav>
+</template>
